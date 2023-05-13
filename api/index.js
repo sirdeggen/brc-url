14280de0df498f566a6acf0d1d16c964138ce15a
Darren Kellenschwiler <deggen@kschw.com>
2023-05-12 21:04:53 -0500

    fix root
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 const app = require('express')();
 
-app.get('/:brc', (req, res) => {
-    const { brc } = req.params;
-    if (!brc) return res.status(308).redirect('https://bsv.brc.dev');
-    res.status(308).redirect('https://bsv.brc.dev/' + brc);
-});
+function redirectTraffic(req, res) {
+    try {
+        const brc = req.params["brc"] || '';
+        return res.status(308).redirect('https://bsv.brc.dev/' + brc);
+    } catch (error) {
+        return res.status(308).redirect('https://bsv.brc.dev');
+    }
+}
+
+app.get('/', redirectTraffic);
+app.get('/:brc', redirectTraffic);
 
 module.exports = app;
