36cda08188c21e13eeaeccf77771ce66fddf624c
Darren Kellenschwiler <deggen@kschw.com>
2023-05-12 21:30:26 -0500

    bearer token and key value store
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- api/index.js
+++ api/index.js
@@ -1,15 +1,33 @@
 const app = require('express')();
+const { kv } = require("@vercel/kv");
+const bearerToken = require('express-bearer-token');
 
-function redirectTraffic(req, res) {
+async function redirectTraffic(req, res) {
     try {
         const brc = req.params["brc"] || '';
-        return res.status(308).redirect('https://bsv.brc.dev/' + brc);
+        const path = await kv.get(brc);
+        return res.status(308).redirect('https://bsv.brc.dev' + path);
     } catch (error) {
+        console.log({ error })
         return res.status(308).redirect('https://bsv.brc.dev');
     }
 }
 
+async function setBRCUrl(req, res) {
+    try {
+        if (req["token"] !== process.env.BEARER_TOKEN) return res.status(401).json({ error: 'you need a token' });
+        const { brc, path } = req.body;
+        await kv.set(brc, path);
+        return res.status(201).json({ [brc]: path });
+    } catch (error) {
+        console.log({ error })
+        return res.status(500).end();
+    }
+}
+
+app.use(bearerToken());
 app.get('/', redirectTraffic);
 app.get('/:brc', redirectTraffic);
+app.post('/set/url', setBRCUrl);
 
 module.exports = app;
